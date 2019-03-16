10d5d5d71b3fb4b77a5262be025f8f49aa1c5c59
skrkuromi <41858669+skrkuromi@users.noreply.github.com>
2019-03-16 18:42:04 +0800

    -

--- frontEnd/demo/src/components/DataOperate/upload.js
+++ frontEnd/demo/src/components/DataOperate/upload.js
@@ -17,130 +17,129 @@ class Uploadfile extends Component{
     }
     
     makeFile = ()=>{
-        var allData = this.state.dataSet[0]
-    
-        var fieldNameArray = this.state.labelArray[0]
-    
-        var vectorlength = this.state.length[0]
-        
-        var newData = new Array();
-    
-        for(let indexOfRows = 0;indexOfRows < vectorlength; indexOfRows++){
-          var row = new Array();
-          for(let indexOfCols = 0; indexOfCols < fieldNameArray.length; indexOfCols++ ){
-              row.push(allData[fieldNameArray[indexOfCols]][indexOfRows])
-          }
-          newData.push(row);
+      var allData = this.state.dataSet[0]
+  
+      var fieldNameArray = this.state.labelArray[0]
+  
+      var vectorlength = this.state.length[0]
+      
+      var newData = new Array();
+  
+      for(let indexOfRows = 0;indexOfRows < vectorlength; indexOfRows++){
+        var row = new Array();
+        for(let indexOfCols = 0; indexOfCols < fieldNameArray.length; indexOfCols++ ){
+            row.push(allData[fieldNameArray[indexOfCols]][indexOfRows])
         }
-        var csv = Papa.unparse({
-          "fields": fieldNameArray,
-          "data": newData
-        });
-    
-        this.setState({
-          finalData:csv
-        })
-    
+        newData.push(row);
+      }
+      var csv = Papa.unparse({
+        "fields": fieldNameArray,
+        "data": newData
+      });
+  
+      this.setState({
+        finalData:csv
+      })
       }
     readFile = (e)=>{
-        var files = e.target.files;
-        //var files = e; // FileList object
-        console.log(files)
-        var reader = new FileReader();
-        const { propsAPI } = this.props;
-        const { getSelected, update, executeCommand } = propsAPI;
-        reader.readAsText(files[0],'gbk');
-        reader.onload = function(e) {
-          var fieldNameArray = [];
-          let vectorLength;
+      var files = e.target.files;
+      //var files = e; // FileList object
+      console.log(files)
+      var reader = new FileReader();
+      const { propsAPI } = this.props;
+      const { getSelected, update } = propsAPI;
+      reader.readAsText(files[0],'gbk');
+      reader.onload = function(e) {
+        var fieldNameArray = [];
+        let vectorLength;
 
-          var results = Papa.parse(e.target.result,{header:true,dynamicTyping: true});
-          fieldNameArray.push(results.meta.fields);
-          vectorLength = results.data.length - 1
-          var n = new Array();
+        var results = Papa.parse(e.target.result,{header:true,dynamicTyping: true});
+        fieldNameArray.push(results.meta.fields);
+        vectorLength = results.data.length - 1
+        var n = new Array();
 
-          for(let indexOfCols = 0; indexOfCols < fieldNameArray[0].length; indexOfCols++){
-              var colName = fieldNameArray[0][indexOfCols];
-              var colValue = new Array();
-              for (let indexOfRows = 0; indexOfRows < results.data.length - 1; indexOfRows++){
-              colValue.push(results.data[indexOfRows][colName])
+        for(let indexOfCols = 0; indexOfCols < fieldNameArray[0].length; indexOfCols++){
+            var colName = fieldNameArray[0][indexOfCols];
+            var colValue = new Array();
+            for (let indexOfRows = 0; indexOfRows < results.data.length - 1; indexOfRows++){
+            colValue.push(results.data[indexOfRows][colName])
+            }
+            n.push({label:colName,value:colValue})
+        }
+        // console.log(n)
+        // console.log(fieldNameArray)
+        // console.log(vectorLength)
+        
+        // SeprtbyFeat();
+
+        var temp = null;
+        var max,min,media,total,tep;
+        var c = 0;
+        for(let i = 0;i < n.length; i++){   
+          for(let j = 0; j < n[i].value.length;j++){
+              if(n[i].value[j] != null && typeof(n[i].value[j]) == "number"){
+                max=min=n[i].value[j];
+                temp = 1;
+                break;
               }
-              n.push([{label:colName,value:colValue}])
           }
-          console.log(n)
-          console.log(fieldNameArray)
-          console.log(vectorLength)
-          
-          // SeprtbyFeat();
-
-          var temp = null;
-          var max,min,media,total,tep;
-          var c = 0;
-          for(let i = 0;i < n.length; i++){   
-            for(let j = 0; j < n[i][0].value.length;j++){
-                if(n[i][0].value[j] != null && typeof(n[i][0].value[j]) == "number"){
-                  max=min=n[i][0].value[j];
-                  temp = 1;
-                  break;
-                }
-            }
-            if(temp != 1){
-              stat[c] = new Array();
-              stat[c]['label'] = fieldNameArray[0][i];
-              stat[c]['value'] = new Array();
-              var w;
-              for(let j = 0; j < n[i][0].value.length;j++){
-                tep = 0;
-                for(w =0; w < stat[c]['value'].length; w++){
-                  if(stat[c]['value'][w].name == n[i][0].value[j]){
-                    tep = 1;
-                    break;
-                  }
-                }
-                if(tep == 1){
-                  stat[c]['value'][w].count++;
-                }
-                else{
-                  stat[c]['value'][w] = new Array();
-                  stat[c]['value'][w]['name'] = n[i][0].value[j];
-                  stat[c]['value'][w]['count'] = 1;
-                }
-              }
-              stat[c]['type'] = 'string';
-              c++;
-              temp = null;
-              continue;
-            }
-            total=0;
-            for(let j = 0; j < n[i][0].value.length;j++){
-              if(n[i][0].value[j] > max) max = n[i][0].value[j];
-              if(n[i][0].value[j] != null && n[i][0].value[j] < min) min = n[i][0].value[j];
-              total+=n[i][0].value[j];
-            }
-            let med = new Array;
-            for(let j = 0; j < n[i][0].value.length;j++){
-              med[j] = n[i][0].value[j];
-            }
-            for(let j = 0; j < n[i][0].value.length;j++){
-              for(let k = j+1; k < n[i][0].value.length;k++){
-                if(med[j] > med[k]){
-                  tep = med[j];
-                  med[j] = med[k];
-                  med[k] = tep;
-                }
-              }
-            }
-            tep = Math.floor(n[i][0].value.length/2);
+          if(temp != 1){
             stat[c] = new Array();
             stat[c]['label'] = fieldNameArray[0][i];
-            stat[c]['max']  = max;
-            stat[c]['min'] = min;
-            stat[c]['average'] = total/n[i][0].value.length;
-            stat[c]['media'] = med[tep];
-            stat[c]['type'] = 'number';
+            stat[c]['value'] = new Array();
+            var w;
+            for(let j = 0; j < n[i].value.length;j++){
+              tep = 0;
+              for(w =0; w < stat[c]['value'].length; w++){
+                if(stat[c]['value'][w].name == n[i].value[j]){
+                  tep = 1;
+                  break;
+                }
+              }
+              if(tep == 1){
+                stat[c]['value'][w].count++;
+              }
+              else{
+                stat[c]['value'][w] = new Array();
+                stat[c]['value'][w]['name'] = n[i].value[j];
+                stat[c]['value'][w]['count'] = 1;
+              }
+            }
+            stat[c]['type'] = 'string';
             c++;
+            temp = null;
+            continue;
           }
-          // console.log(stat)
+          total=0;
+          for(let j = 0; j < n[i].value.length;j++){
+            if(n[i].value[j] > max) max = n[i].value[j];
+            if(n[i].value[j] != null && n[i].value[j] < min) min = n[i].value[j];
+            total+=n[i].value[j];
+          }
+          let med = new Array;
+          for(let j = 0; j < n[i].value.length;j++){
+            med[j] = n[i].value[j];
+          }
+          for(let j = 0; j < n[i].value.length;j++){
+            for(let k = j+1; k < n[i].value.length;k++){
+              if(med[j] > med[k]){
+                tep = med[j];
+                med[j] = med[k];
+                med[k] = tep;
+              }
+            }
+          }
+          tep = Math.floor(n[i].value.length/2);
+          stat[c] = new Array();
+          stat[c]['label'] = fieldNameArray[0][i];
+          stat[c]['max']  = max;
+          stat[c]['min'] = min;
+          stat[c]['average'] = total/n[i].value.length;
+          stat[c]['media'] = med[tep];
+          stat[c]['type'] = 'number';
+          c++;
+        }
+        // console.log(stat)
           
           let m = fieldNameArray[0].map((item)=>{
             return [item, false];
@@ -149,14 +148,13 @@ class Uploadfile extends Component{
           n['stat'] = stat;
           const values = {
               Dataset:n,
-              labelArray:m, 
+              labelArray:{public:m}, 
               length:vectorLength
           }
           console.log(values);
           const item = getSelected()[0];
           update(item, {...values});
         }
-
     }
     setTest = (allData,fieldNameArray,vectorLength) =>{
         this.setState({
