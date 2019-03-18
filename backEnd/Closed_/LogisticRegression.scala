691fea8cc627a96f6a106ec0451c29f73751c6d4
lightning1734 <34413787+lightning1734@users.noreply.github.com>
2019-03-18 19:49:56 +0800

    Add files via upload

---
+++ backEnd/Closed_/LogisticRegression.scala
@@ -0,0 +1,31 @@
+﻿import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions.col
+
+    val project = "Taitanic1"
+    val id = "6"
+    val train = "MinMaxScaledAge MinMaxScaledPclass Mother SexIndex MinMaxScaledFare"
+    val label = "Survived"
+    val previous = "5"
+    val file = project + "/" + previous
+    val all = train + " " + label
+
+    val df = spark.read.format("json").load(file)
+    val aimarray = all.split(" ")
+    val trainArray = train.split(" ")
+    var df_ = df
+    df_ = df_.select(aimarray.map(A => col(A)): _*)
+
+    val assembler = new VectorAssembler().setInputCols(trainArray).setOutputCol("features")
+    df_ = assembler.transform(df_)
+
+    val lr1 = new LogisticRegression()
+    val lr2 = lr1.setFeaturesCol("features").setLabelCol(label).setFitIntercept(true).setMaxIter(50)
+    val lr = lr2
+
+    val lrModel = lr.fit(df_)
+    lrModel.extractParamMap()
+    println(s"Coefficients: ${lrModel.coefficients} Intercept: ${lrModel.intercept}")
+
+    lrModel.save("Model/" + project + "/" + id)
