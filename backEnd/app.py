c6baabbfe78b4a99bc2eb8982ceb9e1a5c658646
ddrid <2821566237@qq.com>
2019-04-01 18:37:14 +0800

    adsf

--- backEnd/app.py
+++ backEnd/app.py
@@ -7,82 +7,6 @@ import nodeCreate
 global inputData
 global runningData
 
-
-def ArraytoString(Array):
-    String = ""
-
-    for i in range(0, len(Array)):
-        if i == len(Array) - 1:
-            String = String + Array[i]
-        else:
-            String = String + Array[i] + " "
-
-    return String
-
-class nodes:
-
-    def __init__(self, id, label, sourceID, attribute, labelArray):
-        self.id = id
-        self.label = label
-        self.attribute = attribute
-        self.sourceID = sourceID
-        self.labelArray = labelArray
-
-    def matchfunction(self, code):
-        if self.label == "Fillna":
-            data = {'code': code %(self.id, self.sourceID[0]['source'], ArraytoString(self.labelArray['public']), self.attribute['type'])}
-        elif self.label == "MinMaxScaler":
-            data = {'code': code %(self.id, self.sourceID[0]['source'], ArraytoString(self.labelArray['public']))}
-        elif self.label =='localFile':
-            data = {'code': code }
-        elif self.label == 'hdfsFile':
-            data = {'code': code % self.id}
-        elif self.label == 'LogisticRegression':
-            data = {'code': code % (self.id)}
-        elif self.label == "TransformType":
-            data = {'code': code % (self.id, ArraytoString(self.labelArray['public']), self.sourceID[0]['source'], "number")}
-        elif self.label == "Stringindex":
-            data = {'code': code % (self.id, ArraytoString(self.labelArray['public']), self.sourceID[0]['source'])}
-        elif self.label == "SortBy":
-            data = {'code': code % (self.id, self.sourceID[0]['source'], ArraytoString(self.labelArray['public']))}
-        elif self.label == "StandardScaler":
-            data = {'code': code % (self.id, ArraytoString(self.labelArray['public']), self.sourceID[0]['source'])}
-        elif self.label == "QuantileDiscretizer":
-            data = {'code': code % (self.id, ArraytoString(self.labelArray['public']), self.attribute['新生成列名'], self.sourceID[0]['source'], self.attribute['类别数'])}
-        elif self.label == "OneHotEncoding":
-            data = {'code': code % (self.id, ArraytoString(self.labelArray['public']), self.sourceID[0]['source'])}
-        elif self.label == "LinearRegression":
-            data = {'code': code % ()}
-        else:
-            data = None
-
-        return data
-
-    def excuted(self):
-        fileobj = open("./Closed_/" + self.label + ".scala", "r")
-        
-        try:
-            code = fileobj.read()
-        finally:
-            fileobj.close()
-        
-        data_mine = self.matchfunction(code)
-
-        session_url = 'http://10.105.222.90:8998/sessions/2'
-        compute = requests.post(session_url+'/statements', data=json.dumps(data_mine), headers=headers)
-      
-        result_url = host + compute.headers['location']
-
-        r = requests.get(result_url, headers=headers)
-
-        while(True):
-            r = req=ests.get(result_url, headers=headers)
-            if r.json()['state'] == 'available':
-                print("finish")
-                break
-
-        pprint.pprint(r.json())
-
 app = Flask(__name__)
 
 CORS(app)
@@ -94,19 +18,6 @@ def hello_world():
 host = 'http://10.105.222.90:8998'
 headers = {'Content-Type': 'application/json'}
 global loss
-
-def convertPost(data):
-    colName = data['colName'].split(",")
-    s = []
-    for name in colName:
-        temp = {}
-        temp['label'] = name
-        temp['value'] = data[name].split(",")
-        s.append(temp)
-    re = [s]
-    re.append([colName])
-    re.append(len(data[name].split(",")))
-    return re
 
 @app.route('/handleInput', methods=['GET', 'POST']) 
 def handleInput():
@@ -123,7 +34,7 @@ def handleInput():
     print(data['code'])
 
     headers = {'Content-Type': 'application/json'}
-    session_url = 'http://10.105.222.90:8998/sessions/2'
+    session_url = 'http://10.105.222.90:8998/sessions/0'
     compute = requests.post(session_url + '/statements', data=json.dumps(data_mine), headers=headers)
 
     result_url = 'http://10.105.222.90:8998' + compute.headers['location']
--- backEnd/nodeCreate.py
+++ backEnd/nodeCreate.py
@@ -11,23 +11,38 @@ def ArraytoString(Array):
 
 class nodes:
 
-    def __init__(self, id, label, sourceID, attribute):
+    def __init__(self, id, label, sourceID, attribute, labelArray):
         self.id = id
         self.label = label
         self.attribute = attribute
         self.sourceID = sourceID
+        self.labelArray = labelArray
 
     def matchfunction(self, code):
-        if self.label == "Input":
-            data = {'code': code % (self.attribute['sourceFile'], self.id)}
-        elif self.label == "缺失值填充":
-            data = {'code': code % (self.attribute['type'], self.id, ArraytoString(self.sourceID))}
-        elif self.label == "归一化":
-            data = {'code': code % (self.id, ArraytoString(self.sourceID))}
+        if self.label == "Fillna":
+            data = {'code': code %(self.id, self.sourceID[0]['source'], ArraytoString(self.labelArray['public']), self.attribute['type'])}
+        elif self.label == "MinMaxScaler":
+            data = {'code': code %(self.id, self.sourceID[0]['source'], ArraytoString(self.labelArray['public']))}
         elif self.label =='localFile':
-            data = {'code':code }
-        elif self.label =='Fillna':
-            data = {'code':code }
+            data = {'code': code }
+        elif self.label == 'hdfsFile':
+            data = {'code': code % self.id}
+        elif self.label == 'LogisticRegression':
+            data = {'code': code % (self.id)}
+        elif self.label == "TransformType":
+            data = {'code': code % (self.id, ArraytoString(self.labelArray['public']), self.sourceID[0]['source'], "number")}
+        elif self.label == "Stringindex":
+            data = {'code': code % (self.id, ArraytoString(self.labelArray['public']), self.sourceID[0]['source'])}
+        elif self.label == "SortBy":
+            data = {'code': code % (self.id, self.sourceID[0]['source'], ArraytoString(self.labelArray['public']))}
+        elif self.label == "StandardScaler":
+            data = {'code': code % (self.id, ArraytoString(self.labelArray['public']), self.sourceID[0]['source'])}
+        elif self.label == "QuantileDiscretizer":
+            data = {'code': code % (self.id, ArraytoString(self.labelArray['public']), self.attribute['新生成列名'], self.sourceID[0]['source'], self.attribute['类别数'])}
+        elif self.label == "OneHotEncoding":
+            data = {'code': code % (self.id, ArraytoString(self.labelArray['public']), self.sourceID[0]['source'])}
+        elif self.label == "LinearRegression":
+            data = {'code': code % ()}
         else:
             data = None
 
