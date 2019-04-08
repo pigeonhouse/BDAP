c65d89ead137d51088df292a66a6bd867c6668a9
skrkuromi <41858669+skrkuromi@users.noreply.github.com>
2019-04-08 19:05:13 +0800

    -

--- frontEnd/src/components/ItemPanel/Item.js
+++ frontEnd/src/components/ItemPanel/Item.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import withGGEditorContext from '@common/context/GGEditorContext/withGGEditorContext';
 import styles from './index.less'
-import { Menu, Icon, Row,Col } from 'antd';
+import { Menu, Icon, Row, Col } from 'antd';
 const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
 
@@ -9,6 +9,9 @@ class Item extends React.Component {
   constructor(props) {
     super(props);
     this.bindEvent();
+    this.state={
+      isMouseEnter:'no'
+    }
   }
 
   handleMouseDown = () => {
@@ -31,29 +34,35 @@ class Item extends React.Component {
     });
   }
   iconClose=(group)=>{
-    if(group === 'input'){
+    if(group === 'input' && this.state.isMouseEnter === 'yes'){
       return (
         <Col span={4}>
-          <Icon type="close" style={{ cursor:"pointer"}} />
+          <Icon type="close" className={styles.iconCloseStyle} />
         </Col>
       )
     }
+  }
+  handleMouseEnter=()=>{
+    this.setState({isMouseEnter:'yes'})
+  }
+  handleMouseLeave=()=>{
+    this.setState({isMouseEnter:'no'})
   }
   render() {
     const { model } = this.props;
     return (
       <div 
-        style={{ cursor: 'pointer', verticalAlign: 'middle'}} 
+        style={{ cursor: 'pointer' }} 
         onMouseDown={this.handleMouseDown}
+        onMouseEnter={this.handleMouseEnter}
+        onMouseLeave={this.handleMouseLeave}
       >
         <Row style={{paddingLeft:5,fontSize:13, cursor:'move'}}>
-        <Col span={20}>
-          <Icon type='bars'/>
+          <Col span={20}>
+            <Icon type='bars'/>
             <span>{model.label}</span>
-            </Col>
-            
-          {this.iconClose(model.group)}
-         
+          </Col> 
+          {this.iconClose(model.group)}       
         </Row>
       </div>
     );
