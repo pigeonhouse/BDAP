7f452a339f4c365bc2713d63d4249aa9fa732874
HoJ825375293 <47890081+HoJ825375293@users.noreply.github.com>
2019-04-08 22:01:25 +0800

    Update handleInput.scala

--- backEnd/handleInput.scala
+++ backEnd/handleInput.scala
@@ -1,22 +1,9 @@
 import scalaj.http._
-  
+
 val file_location = "/demoData/%s"
 val file_type = "csv"
 val test = spark.read.format(file_type).option("header","true").option("inferSchema", "true").load(file_location)
 
-val colName = test.columns
-val fin = new StringBuilder;
-val start = """{"colName":""""
-val end = "\""
-val json = colName.mkString(start,",",end)
-fin ++= json
-for(name <- colName){
-  var t = test.select(name).takeAsList(20).toArray.mkString.stripSuffix("]").stripPrefix("[").split("\\]\\[")
-  var r = ",\"" + name + "\":\"" + t.mkString(",") + "\""
-  fin ++= r
-}
-fin += '}'
+val fin = test.limit(20).toJSON.collectAsList.toString
 
-println(fin)
-
-val result = Http("http://10.122.240.131:5000/InputPost").postData(fin.toString).header("Content-Type", "application/json").header("Charset", "UTF-8").option(HttpOptions.readTimeout(10000)).asString
+val result = Http("http://10.122.224.119:5000/InputPost").postData(fin.toString).header("Content-Type", "application/json").header("Charset", "UTF-8").option(HttpOptions.readTimeout(10000)).asString
