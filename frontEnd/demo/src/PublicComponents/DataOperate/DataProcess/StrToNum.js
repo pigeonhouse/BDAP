3dab7dc4a46d50da70f7440c3b9b39696010cd4e
HoJ825375293 <47890081+HoJ825375293@users.noreply.github.com>
2019-04-22 11:59:27 +0800

    -

--- frontEnd/demo/src/PublicComponents/DataOperate/DataProcess/StrToNum.js
+++ frontEnd/demo/src/PublicComponents/DataOperate/DataProcess/StrToNum.js
@@ -4,46 +4,39 @@ export function StrToNum(allData){
     var tem, temp;
     var labelArray = new Array();
 
-    console.log(allData);
     for(let i = 0; i < Dataset.length ; i++){
         tem = 0;
-        for(let j = 0; j < allData[0].labelArray.public.length; j++){
-            if(Dataset[i].label == allData[0].labelArray.public[j]){
-                tem = 1;
-            }
-        }
+        for(let j = 0; j < allData[0].labelArray.public.length; j++)
+            if(Dataset[i].label == allData[0].labelArray.public[j])    tem = 1;
         temp = new Array(); 
-        if(tem == 1){
-            temp = [Dataset[i].label, true];
-        }
-        else{
-            temp = [Dataset[i].label, false];
-        }
+        if(tem == 1)    temp = [Dataset[i].label, true];
+        else    temp = [Dataset[i].label, false];
         labelArray.push(temp);
     }
-    // var attr = {id:[['1','mon','tur','sta'],['0','sun']]};
-    // var Dataset = [[{label:'id',value:['mon','tur','sun','sta']}],[{label:'age',value:[1,2,3]}]];
-    // var labelArray = [["id",true],["age",false]];
-    // var stat = [{average:2,max:3,min:2,media:2},{average:2,max:3,min:2,media:2},{average:2,max:3,min:2,media:2}]
     for(let i = 0; i < labelArray.length;i++){
         if(labelArray[i][1] == true){
-            for(let j = 0; j < Dataset[i].value.length; j++){
-                for(let k = 0; k < attr.public.length; k++){
-                    for(let l = 1;l < attr.public[k].length;l++ ){
-                        if(Dataset[i].value[j] == attr.public[k][l]){
-                            Dataset[i].value[j] = attr.public[k][0];
+            if(Dataset[i].stat.type == 'string'){
+                console.log("---------")
+                console.log(attr);
+                for(let j = 0; j < Dataset[i].value.length; j++){
+                    Object.keys(attr).forEach(function(key){
+                        // console.log("77777777777777")
+                        // console.log(attr[key][0])
+                        for(let k = 0; k < attr[key].length; k++){
+                            for(let m = 1; m < attr[key][k].length; m++){
+                                if(Dataset[i].value[j] == attr[key][k][m]){
+                                    Dataset[i].value[j] = attr[key][k][0];
+                                    break;
+                                }
+                            }
                         }
-                    }
+                    })
                 }
             }
         }
     }
-    // console.log("xxxxxxxxxxxxxxxxxxxx");
-    // console.log(labelArray);
-    console.log(Dataset);
+    console.log(Dataset)
 
-    allData.Dataset = Dataset;
-    // allData.labelArray = labelArray1;
-
-    return allData;
+    allData[1].Dataset = Dataset;
+    return allData[1];
 }
