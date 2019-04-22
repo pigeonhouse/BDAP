b98b58e4bca41f314f632a8e20321fdbffa4ca1d
skrkuromi <41858669+skrkuromi@users.noreply.github.com>
2019-04-22 21:54:10 +0800

    -

--- frontEnd/demo/src/PublicComponents/DataOperate/download.js
+++ frontEnd/demo/src/PublicComponents/DataOperate/download.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Icon } from 'antd'
+import { Icon, message } from 'antd'
 import { withPropsAPI } from '@src'
 import Papa from 'papaparse'
 import {
@@ -7,48 +7,47 @@ import {
   } from '@src';
 import styles from '../EditorContextMenu/index.less';
 class Download extends Component{
-    constructor(props){
-        super(props);
-    }
     makeFile = ()=>{
-        console.log('---------')
         const { propsAPI } = this.props;
         const { getSelected } = propsAPI;
         const item = getSelected()[0];
         const model = item.getModel();
-        var allData = model.dataset;
-        var fieldNameArray = model.labelArray;
-        var vectorlength = model.length;
-
-        var newData = new Array();
-
-        for(let indexOfRows = 0;indexOfRows < vectorlength; indexOfRows++){
-            var row = new Array();
-            for(let indexOfCols = 0; indexOfCols < fieldNameArray.length; indexOfCols++ ){
-                row.push(allData[fieldNameArray[indexOfCols]][indexOfRows])
-            }
-            newData.push(row);
+        var allData = model.Dataset;
+        if(allData.length === 0){
+            message.warning('no file!');
         }
-        var csv = Papa.unparse({
-            "fields": fieldNameArray,
-            "data": newData
-        });
-        console.log(csv);
-        this.downFile(csv);
+        else {
+            var fieldNameArray = new Array();
+            var newData = new Array();
+            var length = model.length;
+            for(let i in model.labelArray.public){
+                fieldNameArray.push(model.labelArray.public[i][0]);
+            }
+            for(let i = 0;i < length;i++){
+                var row = new Array();
+                for(let j in allData){
+                    row.push(allData[j].value[i]);
+                }
+                newData.push(row);
+            }
+            var csv = Papa.unparse({
+                "fields": fieldNameArray,
+                "data": newData
+            });
+            this.downFile(csv);
+        }
     }
     downFile = (list)=> {
-        console.log('------')
-        console.log(list)
-        // var elementA = document.createElement('a');
-        // elementA.download = "downLoad.csv";
-        // elementA.style.display = 'none';
-        // var blob = new Blob([list], {
-        //     type: "text/csv;charset="+ 'utf-8' + ";"
-        // });
-        // elementA.href = URL.createObjectURL(blob);
-        // document.body.appendChild(elementA);
-        // elementA.click();
-        // document.body.removeChild(elementA);
+        var elementA = document.createElement('a');
+        elementA.download = "downLoad.csv";
+        elementA.style.display = 'none';
+        var blob = new Blob([list], {
+            type: "text/csv;charset="+ 'utf-8' + ";"
+        });
+        elementA.href = URL.createObjectURL(blob);
+        document.body.appendChild(elementA);
+        elementA.click();
+        document.body.removeChild(elementA);
     }
 
     render(){
