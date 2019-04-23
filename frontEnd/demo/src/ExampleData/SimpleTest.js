82279e9a6807742fba81695f59b42c4ae0f56097
HoJ825375293 <47890081+HoJ825375293@users.noreply.github.com>
2019-04-23 23:09:48 +0800

    -

---
+++ frontEnd/demo/src/ExampleData/SimpleTest.js
@@ -0,0 +1,18 @@
+export const Data = [
+    {"id":24,"age":10,"gender":"male"},
+    {"id":25,"age":12,"gender":"female"},
+    {"id":26,"age":14,"gender":"male"},
+    {"id":27,"age":null,"gender":"male"},
+    {"id":28,"age":18,"gender":"female"},
+    {"id":29,"age":20,"gender":"male"},
+    {"id":30,"age":22,"gender":"male"},
+    {"id":31,"age":24,"gender":"female"},
+    {"id":32,"age":26,"gender":"male"},
+    {"id":33,"age":null,"gender":"male"},
+    {"id":34,"age":30,"gender":"female"},
+    {"id":35,"age":32,"gender":"male"},
+    {"id":36,"age":34,"gender":"male"},
+    {"id":37,"age":36,"gender":"female"},
+    {"id":38,"age":38,"gender":"male"},
+    {"id":39,"age":40,"gender":"male"}
+    ]
--- frontEnd/demo/src/PublicComponents/DataOperate/SimpleTest.js
+++ frontEnd/demo/src/PublicComponents/DataOperate/SimpleTest.js
@@ -1,48 +1,48 @@
-// import React, { Component } from 'react';
-// import { Icon, Button } from 'antd'
-// import Papa from 'papaparse'
-// import { withPropsAPI } from '@src';
-// import { Stat } from './stat';
-// import { Data }from '../../ExampleData/SimpleTest';
-// class SimpleTest extends Component{
-//     readFile = ()=>{
-//         const { propsAPI } = this.props;
-//         const { getSelected, update } = propsAPI;
-//         var fieldNameArray = [];
-//         let vectorLength;
-//         var results = {'data':Data,'meta':{'fields':["Name","Type 1","Type 2","Total","HP","Attack","Defense","Sp. Atk","Sp. Def","Speed","Generation","Legendary"]}};
-//         fieldNameArray.push(results.meta.fields);
-//         vectorLength = results.data.length - 1;
-//         var n = new Array();
+import React, { Component } from 'react';
+import { Icon, Button } from 'antd'
+import Papa from 'papaparse'
+import { withPropsAPI } from '@src';
+import { Stat } from './stat';
+import { Data }from '../../ExampleData/SimpleTest';
+class SimpleTest extends Component{
+    readFile = ()=>{
+        const { propsAPI } = this.props;
+        const { getSelected, update } = propsAPI;
+        var fieldNameArray = [];
+        let vectorLength;
+        var results = {'data':Data,'meta':{'fields':["id","age","gender"]}};
+        fieldNameArray.push(results.meta.fields);
+        vectorLength = results.data.length - 1;
+        var n = new Array();
 
-//         for(let indexOfCols = 0; indexOfCols < fieldNameArray[0].length; indexOfCols++){
-//             var colName = fieldNameArray[0][indexOfCols];
-//             var colValue = new Array();
-//             for (let indexOfRows = 0; indexOfRows < results.data.length - 1; indexOfRows++){
-//             colValue.push(results.data[indexOfRows][colName])
-//             }
-//             n.push({label:colName,value:colValue})
-//         }
-//         var STAT = new Array();
-//         STAT = Stat(n);
-//         let m = fieldNameArray[0].map((item)=>{
-//           return [item, false];
-//         })
-//         var values = {
-//             Dataset:STAT,
-//             labelArray:{public:m}, 
-//             length:vectorLength
-//         }
-//         const item = getSelected()[0];
-//         values['keyConfig'] = JSON.parse(JSON.stringify(item.model.keyConfig));
-//         values.keyConfig.state_icon_url = 'https://gw.alipayobjects.com/zos/rmsportal/MXXetJAxlqrbisIuZxDO.svg';
-//         update(item, {...values});
-//     }
-//     render(){
-//         return(
-//             <div>{this.readFile()}</div>
-//         )
-//     }
-// }
+        for(let indexOfCols = 0; indexOfCols < fieldNameArray[0].length; indexOfCols++){
+            var colName = fieldNameArray[0][indexOfCols];
+            var colValue = new Array();
+            for (let indexOfRows = 0; indexOfRows < results.data.length - 1; indexOfRows++){
+            colValue.push(results.data[indexOfRows][colName])
+            }
+            n.push({label:colName,value:colValue})
+        }
+        var STAT = new Array();
+        STAT = Stat(n);
+        let m = fieldNameArray[0].map((item)=>{
+          return [item, false];
+        })
+        var values = {
+            Dataset:STAT,
+            labelArray:{public:m}, 
+            length:vectorLength
+        }
+        const item = getSelected()[0];
+        values['keyConfig'] = JSON.parse(JSON.stringify(item.model.keyConfig));
+        values.keyConfig.state_icon_url = 'https://gw.alipayobjects.com/zos/rmsportal/MXXetJAxlqrbisIuZxDO.svg';
+        update(item, {...values});
+    }
+    render(){
+        return(
+            <div>{this.readFile()}</div>
+        )
+    }
+}
 
-// export default withPropsAPI(SimpleTest);
+export default withPropsAPI(SimpleTest);
