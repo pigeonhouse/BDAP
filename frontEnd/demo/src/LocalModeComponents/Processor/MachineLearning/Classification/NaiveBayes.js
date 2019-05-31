bbab54862e72d465d09d15b701367fe5ce22997d
skrkuromi <41858669+skrkuromi@users.noreply.github.com>
2019-05-31 17:29:31 +0800

    -

--- frontEnd/demo/src/LocalModeComponents/Processor/MachineLearning/Classification/NaiveBayes.js
+++ frontEnd/demo/src/LocalModeComponents/Processor/MachineLearning/Classification/NaiveBayes.js
@@ -1,1 +1,38 @@
+import { selectData, selectDataUntransport } from '../ToolFunctions//ToolFunctions'
+import { Stat } from '../../../../PublicComponents/DataOperate/stat'
+import { Randis } from '../../../../PublicComponents/DataOperate/DataProcess/Randis'
+import { GaussianNB } from "ml-naivebayes";
+import ConfusionMatrix from 'ml-confusion-matrix';
+function normalize(pre, predictObj, PreArray){
     let Dataset = pre;
+    Dataset.push(Stat([{label:PreArray[0], value:predictObj}])[0]);
+    return {Dataset:Dataset};
+}
+export function NaiveBayes(all_data){
+    var data = JSON.parse(JSON.stringify(all_data));
+    data[0]["all_attr"] = {};
+    data[0].all_attr.public = 0.7;
+    const Dataset = Randis(data).Dataset;
+    const labelArray = all_data[0].labelArray;
+    const trainData = Dataset[0];
+    const textData = all_data[2].Dataset;
+
+    const x = selectData(trainData, labelArray.train_x);
+    const y = selectDataUntransport(trainData, labelArray.train_y);
+    const predict = selectData(textData, labelArray.predict_x);
+
+    const testx = selectData(Dataset[1], labelArray.train_x);
+    const testy = selectDataUntransport(Dataset[1], labelArray.train_y);
+
+    const model = new GaussianNB();
+    model.train(x, y);
+    var pre = model.predict(testx)
+    
+    const CM2 = ConfusionMatrix.fromLabels(testy, pre);
+
+    var predictObj = model.predict(predict);
+
+    var resultData = normalize(textData, predictObj, labelArray.predict_y);
+    resultData["evaluation"]=[["Precision",CM2.getAccuracy(),"正确率"]];
+    return resultData;
+}  
---
+++ frontEnd/demo/src/LocalModeComponents/Processor/MachineLearning/Regression/RandomForest.js
@@ -0,0 +1,29 @@
+import { RandomForestRegression as RFRegression } from 'ml-random-forest';
+import {selectData, selectDataUntransport} from '../ToolFunctions//ToolFunctions'
+import { Stat } from '../../../../PublicComponents/DataOperate/stat'
+
+function normalize(pre, predictObj, PreArray){
+    let Dataset = pre;
+    Dataset.push(Stat([{label:PreArray[0], value:predictObj}])[0]);
+    return {Dataset:Dataset};
+}
+export function RandomForest(all_data){
+    const labelArray = all_data[0].labelArray;
+    const trainData = all_data[1].Dataset;
+    const textData = all_data[2].Dataset;
+    const attr = all_data[0].all_attr;
+
+    const x = selectData(trainData, labelArray.train_x);
+    const y = selectDataUntransport(trainData, labelArray.train_y);
+    const predict = selectData(textData, labelArray.predict_x);
+    const options = {
+        seed: 3, 
+        maxFeatures: 2,
+        replacement: false,
+        nEstimators: 200
+    }
+    var regression = new RFRegression(options);
+    regression.train(x, y);
+    const predictObj = regression.predict(predict);
+    return normalize(textData, predictObj, labelArray.predict_y)
+}
