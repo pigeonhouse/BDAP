e06e05862aaec3f75fa1c33064391fdafae3b077
skrkuromi <987186163@qq.com>
2019-08-07 10:06:18 +0800

    -

---
+++ frontEnd/demo/src/LocalModeComponents/EditorItemPanel/FlowItemModel.js
@@ -0,0 +1,252 @@
+export const localItem = {
+    defaultOpenKeys:['sub2','sub3'], mode:"inline", selectable:false, subMenu:[{
+        key:"sub2",type:"mail",name:"数据预处理",menu:[{
+            type:"node",
+            size:"200*40",
+            shape:"one-one",
+            model:{
+                label: '缺失值填充',
+                elabel:'Fillna',
+                anchor: [1, 1],
+                attr:{type:'average'},
+                attrDetail:[],
+                // attrDetail:[{elabel:'type',label:'填充值', type:'Input', regexp:'^[0-9]+.?[0-9]*'}],
+                // attrDetail:[{elabel:'type',label:'填充值', type:'Select', evalue:['average', 'median', 'max', 'min'], value:['平均值', '中位数', '最大值', '最小值']}],
+                Dataset: [],
+                labelArray: {}, 
+                length: 0,
+                group: 'feature',
+                keyConfig:{
+                    color_type: '#1890FF',
+                    state_icon_url: 'https://gw.alipayobjects.com/zos/rmsportal/uZVdwjJGqDooqKLKtvGA.svg',
+                }
+            }
+        },{
+            type:"node",
+            size:"200*40",
+            shape:"one-one",
+            model:{
+              label: '归一化',
+              elabel:'MinMaxScaler',
+              attr:{},
+              attrDetail:[],
+              Dataset: [],
+              labelArray: {}, 
+              length: 0,
+              anchor: [1, 1],
+              group: 'feature',
+              keyConfig:{
+                color_type: '#1890FF',
+                state_icon_url: 'https://gw.alipayobjects.com/zos/rmsportal/uZVdwjJGqDooqKLKtvGA.svg',
+              }
+            }
+        },{
+            type:"node",
+            size:"200*40",
+            shape:"one-one",
+            model:{
+              label: '特征区间化',
+              attr:{},
+              attrDetail:[],
+              Dataset: [],
+              labelArray: {}, 
+              length: 0,
+              anchor: [1, 1],
+              group: 'feature',
+              keyConfig:{
+                color_type: '#1890FF',
+                state_icon_url: 'https://gw.alipayobjects.com/zos/rmsportal/uZVdwjJGqDooqKLKtvGA.svg',
+              }
+            }
+        },{
+            type:"node",
+            size:"200*40",
+            shape:"one-one",
+            model:{
+                label: '特征分组归类',
+                attr:{},
+                attrDetail:[],
+                Dataset: [],
+                labelArray: {}, 
+                length: 0,
+                anchor: [1, 1],
+                group: 'feature',
+                keyConfig:{
+                color_type: '#1890FF',
+                state_icon_url: 'https://gw.alipayobjects.com/zos/rmsportal/uZVdwjJGqDooqKLKtvGA.svg',
+                }
+            }
+        },{
+            type:"node",
+            size:"200*40",
+            shape:"one-one",
+            model:{
+              label: 'one-hot编码',
+              attr:{},
+              attrDetail:[],
+              Dataset: [],
+              labelArray: {}, 
+              length: 0,
+              anchor: [1, 1],
+              group: 'feature',
+              keyConfig:{
+                color_type: '#1890FF',
+                state_icon_url: 'https://gw.alipayobjects.com/zos/rmsportal/uZVdwjJGqDooqKLKtvGA.svg',
+              }
+            }
+        },{
+            type:"node",
+            size:"200*40",
+            shape:"one-two",
+            model:{
+              label: '数据随机划分',
+              attr:{public:0.7},
+              attrDetail:[],
+              Dataset: [],
+              labelArray: {}, 
+              length: 0,
+              anchor: [1, 2],
+              group: 'feature',
+              keyConfig:{
+                color_type: '#1890FF',
+                state_icon_url: 'https://gw.alipayobjects.com/zos/rmsportal/uZVdwjJGqDooqKLKtvGA.svg',
+              }
+            }
+        }]
+    }
+    , {
+        key:"sub3", type:"appstore", name:"机器学习", subMenu:[{
+            key:"g3", title:"回归",menu:[{
+                type:"node",
+                size:"200*40",
+                shape:'two-one',
+                model:{
+                    label: '单变量线性回归',
+                    anchor: [2, 1],
+                    attr:{},
+                    attrDetail:[],
+                    Dataset: [],
+                    labelArray: {}, 
+                    length: 0,
+                    group:"ml",
+                    evaluation:[],
+                    keyConfig:{
+                        color_type: '#1890FF',
+                        state_icon_url: 'https://gw.alipayobjects.com/zos/rmsportal/uZVdwjJGqDooqKLKtvGA.svg',
+                    }
+                }
+            },{
+                type:"node",
+                size:"200*40",
+                shape:'two-one',
+                model:{
+                    label: '单变量多项式回归',
+                    attr:{'多项式最高幂': 5},
+                    attrDetail:[{elabel:'多项式最高幂',label:'多项式最高幂', type:'Input', regexp:'^[1-9][0-9]*$'}],
+                    anchor: [2, 1],
+                    Dataset: [],
+                    labelArray: {},
+                    length: 0,
+                    group:"ml",
+                    keyConfig:{
+                        color_type: '#1890FF',
+                        state_icon_url: 'https://gw.alipayobjects.com/zos/rmsportal/uZVdwjJGqDooqKLKtvGA.svg',
+                    }
+                }    
+            },{
+                type:"node",
+                size:"200*40",
+                shape:'two-one',
+                model:{
+                  label: '多变量线性回归',
+                  attr:{},
+                  attrDetail:[],
+                  group:"ml",
+                  anchor: [2, 1],
+                  Dataset: [],
+                  labelArray: {}, 
+                  length: 0,
+                  keyConfig:{
+                    color_type: '#1890FF',
+                    state_icon_url: 'https://gw.alipayobjects.com/zos/rmsportal/uZVdwjJGqDooqKLKtvGA.svg',
+                  }
+                }
+            },{
+                type:"node",
+                size:"200*40",
+                shape:'two-one',
+                model:{
+                    label: '决策树回归',
+                    attr:{},
+                    attrDetail:[],
+                    anchor: [2, 1],
+                    Dataset: [],
+                    labelArray: {}, 
+                    length: 0,
+                    group:"ml",
+                    keyConfig:{
+                        color_type: '#1890FF',
+                        state_icon_url: 'https://gw.alipayobjects.com/zos/rmsportal/uZVdwjJGqDooqKLKtvGA.svg',
+                    }
+                }
+            },{
+                type:"node",
+                size:"200*40",
+                shape:'two-one',
+                model:{
+                  label: '随机森林回归',
+                  attr:{},
+                  attrDetail:[],
+                  anchor: [2, 1],
+                  Dataset: [],
+                  labelArray: {}, 
+                  length: 0,
+                  group:"ml",
+                  keyConfig:{
+                    color_type: '#1890FF',
+                    state_icon_url: 'https://gw.alipayobjects.com/zos/rmsportal/uZVdwjJGqDooqKLKtvGA.svg',
+                  }
+                }
+            }]
+        }, {
+            key:"g4", title:"分类", menu:[{
+                type:"node",
+                size:"200*40",
+                shape:'two-one',
+                model:{
+                    label: '朴素贝叶斯',
+                    attr:{},
+                    attrDetail:[],
+                    anchor: [2, 1],
+                    Dataset: [],
+                    labelArray: {}, 
+                    length: 0,
+                    group:"ml",
+                    keyConfig:{
+                    color_type: '#1890FF',
+                    state_icon_url: 'https://gw.alipayobjects.com/zos/rmsportal/uZVdwjJGqDooqKLKtvGA.svg',
+                  }
+                }   
+            },{
+                type:"node",
+                size:"200*40",
+                shape:'two-one',
+                model:{
+                  label: '支持向量机',
+                  attr:{},
+                  attrDetail:[],
+                  anchor: [2, 1],
+                  Dataset: [],
+                  labelArray: {}, 
+                  length: 0,
+                  group:"ml",
+                  keyConfig:{
+                    color_type: '#1890FF',
+                    state_icon_url: 'https://gw.alipayobjects.com/zos/rmsportal/uZVdwjJGqDooqKLKtvGA.svg',
+                  }
+                }
+            }]
+        }]
+    }
+]
+} 
--- frontEnd/demo/src/LocalModeComponents/EditorItemPanel/FlowItemPanel.js
+++ frontEnd/demo/src/LocalModeComponents/EditorItemPanel/FlowItemPanel.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { ItemPanel, Item } from '@src';
 import { Menu, Icon } from 'antd';
-import ItemDecoration from '../../PublicComponents/ItemDecoration/ItemDecoration'
+import ItemDecoration from '../../PublicComponents/ItemDecoration/ItemDecoration';
 import styles from './index.less';
+import { localItem } from './FlowItemModel';
 
 /**
  * 左侧下拉菜单栏，包括可操作实现的组件
@@ -11,7 +12,8 @@ const SubMenu = Menu.SubMenu;
 
 class FlowItemPanel extends React.Component {
   state={
-    isMouseEnter:false
+    isMouseEnter:false,
+    itemNumber:1
   }
   mouseEnter=()=>{
     this.setState({isMouseEnter:true})
@@ -19,299 +21,67 @@ class FlowItemPanel extends React.Component {
   mouseLeave=()=>{
     this.setState({isMouseEnter:false})
   }
+  createItemPanel=(item)=>{
+    var result = [];
+    // 判断是否有SubMenu，没有则跳过，有则加入并递归下一层SubMenu
+    if(item.hasOwnProperty("subMenu")){
+      var subMenu;
+      for(let i in item.subMenu){
+        subMenu = item.subMenu[i];
+        // 判断两种SubMenu样式类型，即是否有Icon，有的话用type及name表示，没有的话用title表示。
+        if(subMenu.title === undefined){
+          result.push(
+            <SubMenu 
+              key={subMenu.key}
+              title={<span><Icon type={subMenu.type} /><span>{subMenu.name}</span></span>}
+            >
+              {this.createItemPanel(subMenu)}
+            </SubMenu>);
+        }
+        else{
+          result.push(
+            <SubMenu 
+              key={subMenu.key}
+              title={subMenu.title}
+            >
+              {this.createItemPanel(subMenu)}
+            </SubMenu>);
+        }
+      }
+    }
+
+    // 判断是否有MenuItem，有则加入，没有则跳过
+    if(item.hasOwnProperty("menu")){
+      var menu;
+      for(let i in item.menu){
+        menu = item.menu[i];
+        result.push(<Menu.Item key={i}><ItemPanel>
+          <Item
+            type={menu.type}
+            size={menu.size}
+            shape={menu.shape}
+            model={menu.model}
+          />
+        </ItemPanel></Menu.Item>);
+      }
+    }
+    return result;
+  }
   render() {
     return (
       <div onMouseEnter={this.mouseEnter} onMouseLeave={this.mouseLeave} 
-      className={this.state.isMouseEnter?styles.scrollapp:styles.unscrollapp}
-      style={{backgroundColor:'#fff'}}>
+        className={this.state.isMouseEnter?styles.scrollapp:styles.unscrollapp}
+        style={{backgroundColor:'#fff'}}>
         <Menu
-            defaultOpenKeys={['sub2','sub3']}
-            mode="inline"
+            defaultOpenKeys={localItem.defaultOpenKeys}
+            mode={localItem.mode}
             style={{maxHeight:'calc(100vh - 105px)', width:'245px', borderRight:0}}
-            selectable={false}
+            selectable={localItem.selectable}
           >
           <ItemDecoration/>
-          <SubMenu key="sub2" title={<span><Icon type="mail" /><span>数据预处理</span></span>}>
-          <Menu.Item key="7">
-              <ItemPanel><Item
-                type="node"
-                size="200*40"
-                shape="one-one"
-                model={{
-                  label: '缺失值填充',
-                  elabel:'Fillna',
-                  anchor: [1, 1],
-                  attr:{type:'average'},
-                  attrDetail:[],
-                  // attrDetail:[{elabel:'type',label:'填充值', type:'Input', regexp:'^[0-9]+.?[0-9]*'}],
-                  // attrDetail:[{elabel:'type',label:'填充值', type:'Select', evalue:['average', 'median', 'max', 'min'], value:['平均值', '中位数', '最大值', '最小值']}],
-                  Dataset: [],
-                  labelArray: {}, 
-                  length: 0,
-                  group: 'feature',
-                  keyConfig:{
-                    color_type: '#1890FF',
-                    state_icon_url: 'https://gw.alipayobjects.com/zos/rmsportal/uZVdwjJGqDooqKLKtvGA.svg',
-                  }
-                }}      
-          /></ItemPanel></Menu.Item>
-
-          <Menu.Item key="8">
-            <ItemPanel><Item
-                type="node"
-                size="200*40"
-                shape="one-one"
-                model={{
-                  label: '归一化',
-                  elabel:'MinMaxScaler',
-                  attr:{},
-                  attrDetail:[],
-                  Dataset: [],
-                  labelArray: {}, 
-                  length: 0,
-                  anchor: [1, 1],
-                  group: 'feature',
-                  keyConfig:{
-                    color_type: '#1890FF',
-                    state_icon_url: 'https://gw.alipayobjects.com/zos/rmsportal/uZVdwjJGqDooqKLKtvGA.svg',
-                  }
-                }}
-              /></ItemPanel></Menu.Item>
-
-              <Menu.Item key="9">
-                <ItemPanel><Item
-                type="node"
-                size="200*40"
-                shape="one-one"
-                model={{
-                  label: '特征区间化',
-                  attr:{},
-                  attrDetail:[],
-                  Dataset: [],
-                  labelArray: {}, 
-                  length: 0,
-                  anchor: [1, 1],
-                  group: 'feature',
-                  keyConfig:{
-                    color_type: '#1890FF',
-                    state_icon_url: 'https://gw.alipayobjects.com/zos/rmsportal/uZVdwjJGqDooqKLKtvGA.svg',
-                  }
-                }}
-              /></ItemPanel></Menu.Item>
-
-              <Menu.Item key="10">
-                <ItemPanel><Item
-                type="node"
-                size="200*40"
-                shape="one-one"
-                model={{
-                  label: '特征分组归类',
-                  attr:{},
-                  attrDetail:[],
-                  Dataset: [],
-                  labelArray: {}, 
-                  length: 0,
-                  anchor: [1, 1],
-                  group: 'feature',
-                  keyConfig:{
-                    color_type: '#1890FF',
-                    state_icon_url: 'https://gw.alipayobjects.com/zos/rmsportal/uZVdwjJGqDooqKLKtvGA.svg',
-                  }
-                }}    
-              /></ItemPanel></Menu.Item>
-
-              <Menu.Item key="11">
-                <ItemPanel><Item
-                type="node"
-                size="200*40"
-                shape="one-one"
-                model={{
-                  label: 'one-hot编码',
-                  attr:{},
-                  attrDetail:[],
-                  Dataset: [],
-                  labelArray: {}, 
-                  length: 0,
-                  anchor: [1, 1],
-                  group: 'feature',
-                  keyConfig:{
-                    color_type: '#1890FF',
-                    state_icon_url: 'https://gw.alipayobjects.com/zos/rmsportal/uZVdwjJGqDooqKLKtvGA.svg',
-                  }
-                }}           
-              /></ItemPanel></Menu.Item>
-              <Menu.Item key="12">
-                <ItemPanel><Item
-                type="node"
-                size="200*40"
-                shape="one-two"
-                model={{
-                  label: '数据随机划分',
-                  attr:{public:0.7},
-                  attrDetail:[],
-                  Dataset: [],
-                  labelArray: {}, 
-                  length: 0,
-                  anchor: [1, 2],
-                  group: 'feature',
-                  keyConfig:{
-                    color_type: '#1890FF',
-                    state_icon_url: 'https://gw.alipayobjects.com/zos/rmsportal/uZVdwjJGqDooqKLKtvGA.svg',
-                  }
-                }}           
-              /></ItemPanel></Menu.Item>
-
-          </SubMenu>
-
-          <SubMenu key="sub3" title={<span><Icon type="appstore" /><span>机器学习</span></span>}>
-            <SubMenu key="g3" title="回归">
-            
-              <Menu.Item key="13"><ItemPanel><Item
-                  type="node"
-                  size="200*40"
-                  shape='two-one'
-                  model={{
-                    label: '单变量线性回归',
-                    anchor: [2, 1],
-                    attr:{},
-                    attrDetail:[],
-                    Dataset: [],
-                    labelArray: {}, 
-                    length: 0,
-                    group:"ml",
-                    evaluation:[],
-                    keyConfig:{
-                      color_type: '#1890FF',
-                      state_icon_url: 'https://gw.alipayobjects.com/zos/rmsportal/uZVdwjJGqDooqKLKtvGA.svg',
-                    }
-                  }}
-                  
-                /></ItemPanel></Menu.Item>
-              <Menu.Item key="14"><ItemPanel><Item
-                  type="node"
-                  size="200*40"
-                  shape='two-one'
-                  model={{
-                    label: '单变量多项式回归',
-                    attr:{'多项式最高幂': 5},
-                    attrDetail:[{elabel:'多项式最高幂',label:'多项式最高幂', type:'Input', regexp:'^[1-9][0-9]*$'}],
-                    anchor: [2, 1],
-                    Dataset: [],
-                    labelArray: {},
-                    length: 0,
-                    group:"ml",
-                    keyConfig:{
-                      color_type: '#1890FF',
-                      state_icon_url: 'https://gw.alipayobjects.com/zos/rmsportal/uZVdwjJGqDooqKLKtvGA.svg',
-                    }
-                  }}            
-                /></ItemPanel></Menu.Item>
-
-                <Menu.Item key="15"><ItemPanel><Item
-                  type="node"
-                  size="200*40"
-                  shape='two-one'
-                  model={{
-                    label: '多变量线性回归',
-                    attr:{},
-                    attrDetail:[],
-                    group:"ml",
-                    anchor: [2, 1],
-                    Dataset: [],
-                    labelArray: {}, 
-                    length: 0,
-                    keyConfig:{
-                      color_type: '#1890FF',
-                      state_icon_url: 'https://gw.alipayobjects.com/zos/rmsportal/uZVdwjJGqDooqKLKtvGA.svg',
-                    }
-                  }}           
-                /></ItemPanel></Menu.Item>
-
-                <Menu.Item key="16"><ItemPanel><Item
-                  type="node"
-                  size="200*40"
-                  shape='two-one'
-                  model={{
-                    label: '决策树回归',
-                    attr:{},
-                    attrDetail:[],
-                    anchor: [2, 1],
-                    Dataset: [],
-                    labelArray: {}, 
-                    length: 0,
-                    group:"ml",
-                    keyConfig:{
-                      color_type: '#1890FF',
-                      state_icon_url: 'https://gw.alipayobjects.com/zos/rmsportal/uZVdwjJGqDooqKLKtvGA.svg',
-                    }
-                  }}              
-                /></ItemPanel></Menu.Item>
-                <Menu.Item key="17"><ItemPanel><Item
-                  type="node"
-                  size="200*40"
-                  shape='two-one'
-                  model={{
-                    label: '随机森林回归',
-                    attr:{},
-                    attrDetail:[],
-                    anchor: [2, 1],
-                    Dataset: [],
-                    labelArray: {}, 
-                    length: 0,
-                    group:"ml",
-                    keyConfig:{
-                      color_type: '#1890FF',
-                      state_icon_url: 'https://gw.alipayobjects.com/zos/rmsportal/uZVdwjJGqDooqKLKtvGA.svg',
-                    }
-                  }}           
-                /></ItemPanel></Menu.Item>         
-            </SubMenu>
-
-            <SubMenu key="g4" title="分类">
-              <Menu.Item key="18"><ItemPanel><Item
-                type="node"
-                size="200*40"
-                shape='two-one'
-                model={{
-                  label: '朴素贝叶斯',
-                  attr:{},
-                  attrDetail:[],
-                  anchor: [2, 1],
-                  Dataset: [],
-                  labelArray: {}, 
-                  length: 0,
-                  group:"ml",
-                  keyConfig:{
-                    color_type: '#1890FF',
-                    state_icon_url: 'https://gw.alipayobjects.com/zos/rmsportal/uZVdwjJGqDooqKLKtvGA.svg',
-                  }
-                }}   
-              /></ItemPanel></Menu.Item>
-
-              <Menu.Item key="19"><ItemPanel><Item
-                type="node"
-                size="200*40"
-                shape='two-one'
-                model={{
-                  label: '支持向量机',
-                  attr:{},
-                  attrDetail:[],
-                  anchor: [2, 1],
-                  Dataset: [],
-                  labelArray: {}, 
-                  length: 0,
-                  group:"ml",
-                  keyConfig:{
-                    color_type: '#1890FF',
-                    state_icon_url: 'https://gw.alipayobjects.com/zos/rmsportal/uZVdwjJGqDooqKLKtvGA.svg',
-                  }
-                }}    
-              /></ItemPanel></Menu.Item>
-            </SubMenu>
-          </SubMenu>
-        </Menu>
-      
-      </div> 
+          {this.createItemPanel(localItem)}
+        </Menu>    
+      </div>
     );
   }
 }
