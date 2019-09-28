44d05fb12e7bb44d3ae56137310fb945afd2ab6c
Halloweenwx <35297693+Halloweenwx@users.noreply.github.com>
2019-09-28 21:04:39 +0800

    fix

--- bdap/src/main/java/com/pigeonhouse/bdap/controller/RunningStatus.java
+++ bdap/src/main/java/com/pigeonhouse/bdap/controller/RunningStatus.java
@@ -1,5 +1,6 @@
 package com.pigeonhouse.bdap.controller;
 
+import com.pigeonhouse.bdap.util.token.PassToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
  * spark向后端发送进度
  * @author HouWeiying
  */
+
 @RestController
 public class RunningStatus {
+    @PassToken
     @PostMapping(value = "/runningStatus")
     public void showStatus(@RequestBody String saying){
         System.out.println(saying);
