049056fd02ecd9a0b3ce18382933eaf6a5d69bfa
skrkuromi <987186163@qq.com>
2019-10-09 11:48:05 +0800

    -

--- frontEnd/demo/src/PublicComponents/VisualDataSource/index.js
+++ frontEnd/demo/src/PublicComponents/VisualDataSource/index.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import { Button } from 'antd';
 
-class DataSource extends React.Component{
-    render(){
-        return(
-            <Button>DataSource</Button>
+class DataSource extends React.Component {
+    handleChangeDataSource = () => {
+        const init = {
+            method: 'POST',
+            mode: 'cors',
+            body: 'filePath=' + '/simpleTest.csv',
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded;charset=utf-8"
+            },
+            credentials: 'include'
+        }
+        fetch('https://result.eolinker.com/MSwz6fu34b763a21e1f7efa84a86a16f767a756952d0f95?uri=localhost:8888/query/readyForData', init)
+            .then(res => {
+                if (res.status === 200) {
+                    console.log(res);
+                    res.text().then(res => {
+                        console.log(res);
+                        if (res.code === 200) {
+                            
+                        }
+                    })
+                }
+            })
+    }
+
+    render() {
+        return (
+            <Button onClick={this.handleChangeDataSource} >DataSource</Button>
         );
     }
 }
