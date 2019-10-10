6bb87f5a56b34efabae4265332b73cbaad6e6473
youjiangzhouzi <34651214+youjiangzhouzi@users.noreply.github.com>
2019-10-10 23:09:26 +0800

    文件树

---
+++ frontEnd/demo/src/PublicComponents/DataOperate/FileOperate/HdfsFileTreeModal.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import { Button, Modal, Tree } from 'antd';
+
+const { TreeNode } = Tree;
+
+/**
+ * HDFS 文件树弹窗组件
+ */
+class HdfsFileTreeModal extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            // 文件树数据，格式：array<{key, title, children, isLeaf}>
+            treeData: []
+        };
+    }
+
+    // 组件渲染后加载数据
+    componentDidMount() {
+        this.onLoadData();
+    }
+
+    /**
+     * 异步加载数据
+     * @param treeNode 树节点
+     */
+    onLoadData = treeNode => new Promise((resolve) => {
+        if (treeNode && treeNode.props.children) {
+            resolve();
+            return;
+        }
+        const init = {
+            method: 'GET',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8',
+                Cookie: 'loginToken=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJsaXZ5QWRkciI6IjEwLjEwNS4yMjIuOTA6ODk5OCIsImF1ZCI6IjIwMTcyMTE1MTEiLCJzZXNzaW9uSWQiOjExMCwidXNlcklkIjoiMjAxNzIxMTUxMSJ9.2onDSv-cMXqh31MCA2tV967xU6CwrrZdNGZjZZisOrk',
+            },
+            credentials: 'include',
+        };
+        fetch(`https://result.eolinker.com/MSwz6fu34b763a21e1f7efa84a86a16f767a756952d0f95?uri=localhost:8888/hdfs/${treeNode != null ? treeNode.props.title : ''}`, init).then((res) => {
+            if (res.status === 200) {
+                res.json().then((res) => {
+                    
+                    if (res.code === 201) {
+                        const data = res.data;
+                        if (data) {
+                            // 将多个文件对象转化为文件数组
+                            const childrenTreeData = [];
+                            for (const i in data) {
+                                childrenTreeData.push({
+                                    key: data[i].ModificationTime,
+                                    title: data[i].filename,
+                                    isLeaf: !data[i].isDir,
+                                });
+                            }
+                            // 如果 treeNode 不为空，说明不是第一次加载，则将 childrenTreeData 设置为 treeNode 的 children
+                            if (treeNode) {
+                                treeNode.props.dataRef.children = childrenTreeData;
+                                this.setState({
+                                    treeData: [...this.state.treeData],
+                                });
+                                // 否则就是第一次加载
+                            } else {
+                                this.setState({
+                                    treeData: childrenTreeData,
+                                });
+                            }
+                        }
+                    }
+                });
+            }
+        });
+        resolve();
+    })
+
+    // 递归生成树节点
+    renderTreeNodes = (data) => {
+        // 保存每次生成的树节点
+        const treeNodeArray = [];
+        data.map((item) => {
+            if (item.children) {
+                treeNodeArray.push(
+                    <TreeNode key={item.key} title={item.title} dataRef={item} isLeaf={item.isLeaf}>
+                        {this.renderTreeNodes(item.children)}
+                    </TreeNode>,
+                );
+            } else {
+                treeNodeArray.push(<TreeNode key={item.key} title={item.title} dataRef={item} isLeaf={item.isLeaf} />);
+            }
+        });
+        return treeNodeArray;
+    }
+
+    render() {
+        // 取出父组件传递过来的参数
+        const { title, visible, handleOk, handleCancel } = this.props;
+        return (
+            <Modal
+                title={title}
+                visible={visible}
+                onOk={handleOk}
+                onCancel={handleCancel}
+                footer={[
+                    <Button>标注为常用文件</Button>,
+                    <Button>确定</Button>,
+                    <Button>取消</Button>,
+                ]}
+            >
+                <Tree loadData={this.onLoadData}>{this.renderTreeNodes(this.state.treeData)}</Tree>
+            </Modal>
+        );
+    }
+}
+export default HdfsFileTreeModal;
