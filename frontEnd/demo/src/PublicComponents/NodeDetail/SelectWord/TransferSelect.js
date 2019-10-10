0e4657b034da3544c79b6ae586bc353e15adc9a1
xty <35415330+intellxty@users.noreply.github.com>
2019-10-10 20:36:32 +0800

    Update TransferSelect.js

--- frontEnd/demo/src/PublicComponents/NodeDetail/SelectWord/TransferSelect.js
+++ frontEnd/demo/src/PublicComponents/NodeDetail/SelectWord/TransferSelect.js
@@ -1,8 +1,6 @@
 import React, { Fragment } from 'react';
 import { withPropsAPI } from '@src';
 import { Button, Tooltip, Modal, Transfer, Divider } from 'antd';
-import { findSourceColumnsInfo } from './FindSourceColumnsInfo';
-import { findLabelArray } from './FindLabelArray';
 import styles from './TransferSelect.less'
 
 //只进行transfer的选择，拿到labelArray，选择后执行changLabelArray函数进行修改
@@ -15,15 +13,15 @@ class TransferSelect extends React.Component {
         preTargetKeys: [],
         tooltipsArray: [],
         tooltipVisible: false,
-        sourceColumnsInfo: [],
     }
 
     componentWillMount() {
-        const { item, attribute, propsAPI } = this.props;
         let mockData = [];
         let targetKeys = [];
         let tooltipsArray = [];
+        const { labelArray } = this.props;
 
+<<<<<<< HEAD
         // 拿到sourceColmnsInfo，再去查看attribute.value是否选中了早已更改的label，若有则删除
         const sourceColumnsInfo = findSourceColumnsInfo(item, attribute.style.sourceAnchor, propsAPI);
         const labelArray = findLabelArray(attribute, sourceColumnsInfo);
@@ -40,25 +38,34 @@ class TransferSelect extends React.Component {
                 targetKeys.push(colName);
                 tooltipsArray.push(colName);
             }
+=======
+        //将labelArray转变为transfer所需的形式，分为mockData(存储标题和key值)，targetKeys(最后选中的key值)
+        //将选中的labelArray放入tooltipsArray中将选中的列名展示。
+        for (let i in labelArray) {
+>>>>>>> parent of 9b332a0e... 选择字段
             mockData.push({
-                key: colName,
-                title: colName,
-                description: colName,
-                chosen: selected
+                key: i.toString(),
+                title: labelArray[i][0],
+                description: labelArray[i][0],
+                chosen: labelArray[i][1]
             });
+            if (labelArray[i][1] === true) {
+                targetKeys.push(i.toString());
+                tooltipsArray.push(labelArray[i][0]);
+            }
         }
 
         this.setState({
             mockData,
             targetKeys,
-            tooltipsArray,
-            sourceColumnsInfo,
+            tooltipsArray
         });
     }
 
     handleOk = () => {
         let labelArray = [];
         let tooltipsArray = [];
+<<<<<<< HEAD
         const { sourceColumnsInfo, mockData, targetKeys } = this.state;
 
         // 生成columnsInfo
@@ -74,6 +81,20 @@ class TransferSelect extends React.Component {
         });
 
         this.props.changeLabelArray(labelArray, this.props.index, sourceColumnsInfo);
+=======
+        const mockData = this.state.mockData;
+        const targetKeys = this.state.targetKeys;
+
+        for (let i in mockData) {
+            if (targetKeys.indexOf(mockData[i].key) !== -1) {
+                labelArray.push([mockData[i].title, true]);
+                tooltipsArray.push(mockData[i].title)
+            }
+            else labelArray.push([mockData[i].title, false]);
+        }
+
+        this.props.changeLabelArray(labelArray, this.props.selectLevel);
+>>>>>>> parent of 9b332a0e... 选择字段
         this.setState({
             visible: false,
             tooltipsArray
@@ -117,10 +138,8 @@ class TransferSelect extends React.Component {
     }
 
     render() {
-        const { attribute } = this.props;
         return (
             <Fragment>
-                <p>{attribute.labelName.label}:</p>
                 <Tooltip
                     arrowPointAtCenter
                     visible={this.state.tooltipVisible}
