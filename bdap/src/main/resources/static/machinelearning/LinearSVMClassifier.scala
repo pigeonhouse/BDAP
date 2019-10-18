afd1688072f8d11c448dc3af7472ce7da0e9ef81
lightning1734 <34413787+lightning1734@users.noreply.github.com>
2019-10-18 18:02:08 +0800

    add algorithm

---
+++ bdap/src/main/resources/static/machinelearning/LinearSVMClassifier.scala
@@ -0,0 +1,29 @@
+object LinearSVMClassifier {
+
+  val trainCols: Array[String] = null
+  val labelCols: Array[String] = null
+  val MaxIter: Int = null
+  val Tol: Double = null
+  val RegParam: Double = null
+  val FitIntercept: Boolean = null
+  var input: DataFrame = null
+
+  def main(args: Array[String]): Unit = {
+    import org.apache.spark.ml.feature.VectorAssembler
+    import org.apache.spark.sql.DataFrame
+    import org.apache.spark.sql.functions.col
+    import org.apache.spark.ml.classification.LinearSVC
+
+    val labelCol = labelCols(0)
+    val aimarray = trainCols:+labelCol
+
+    val metaData = input
+    val assembler = new VectorAssembler().setInputCols(trainCols).setOutputCol("features_svm")
+    val assembledDF = assembler.transform(metaData)
+
+    val linearSVC = new LinearSVC().setFeaturesCol("features_svm").setLabelCol(labelCol).setRegParam(RegParam).setMaxIter(MaxIter).setTol(Tol).setFitIntercept(FitIntercept)
+
+    val Model = linearSVC.fit(assembledDF)
+
+  }
+}
