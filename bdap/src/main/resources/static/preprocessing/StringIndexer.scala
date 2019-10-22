92e8ba9e21ae461eae85830cda0455763c54f8ad
BrokenArrow2019 <52489384+BrokenArrow2019@users.noreply.github.com>
2019-10-22 20:29:52 +0800

    Update StringIndexer.scala

--- bdap/src/main/resources/static/preprocessing/StringIndexer.scala
+++ bdap/src/main/resources/static/preprocessing/StringIndexer.scala
@@ -1,12 +1,18 @@
 object StringIndexer {
 
   val input: DataFrame = null
+  val targetCols: Array[String] = null
 
   def main(args: Array[String]): Unit = {
 
     import org.apache.spark.ml.feature.StringIndexer
+    import org.apache.spark.ml.feature.VectorAssembler
 
-    val df = spark.createDataFrame(
+    val metaData = input
+    val assembler = new VectorAssembler().setInputCols(targetCols)
+    val assembled = assembler.transform(metaData)
+
+    input = spark.createDataFrame(
       Seq((0, "a"), (1, "b"), (2, "c"), (3, "a"), (4, "a"), (5, "c"))
     ).toDF("id", "category")
 
@@ -14,7 +20,7 @@ object StringIndexer {
       .setInputCol("category")
       .setOutputCol("categoryIndex")
 
-    val indexed = indexer.fit(df).transform(df)
+    val indexed = indexer.fit(input).transform(input)
 
     val output = indexed
 
