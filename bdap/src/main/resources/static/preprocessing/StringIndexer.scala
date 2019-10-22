1decba696bbf27bc19f4442866897f3543342882
lightning1734 <34413787+lightning1734@users.noreply.github.com>
2019-10-22 20:33:32 +0800

    add algorithm

--- bdap/src/main/resources/static/preprocessing/StringIndexer.scala
+++ bdap/src/main/resources/static/preprocessing/StringIndexer.scala
@@ -2,19 +2,26 @@ object StringIndexer {
 
   val input: DataFrame = null
 
+  val handleInvalid: String = null
+  //Param for how to handle invalid entries.
+
+  val targetCols: Array[String] = null
+  //Param for input column names.
+
+  val stringOrderType: String = null
+
+  val outputCol: String = null
+  //Param for output column names.
+
   def main(args: Array[String]): Unit = {
 
     import org.apache.spark.ml.feature.StringIndexer
 
-    val df = spark.createDataFrame(
-      Seq((0, "a"), (1, "b"), (2, "c"), (3, "a"), (4, "a"), (5, "c"))
-    ).toDF("id", "category")
+    val metaData = input
 
-    val indexer = new StringIndexer()
-      .setInputCol("category")
-      .setOutputCol("categoryIndex")
+    val indexer = new StringIndexer().setInputCol(targetCols).setOutputCol(outputCol).setHandleInvalid(handleInvalid).setStringOrderType(stringOrderType)
 
-    val indexed = indexer.fit(df).transform(df)
+    val indexed = indexer.fit(input).transform(input)
 
     val output = indexed
 
