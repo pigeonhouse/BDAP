a85b0ef8f126d8c2bcca7024fc7681642e8d9c87
liqiuyue1234567 <49329582+liqiuyue1234567@users.noreply.github.com>
2019-10-24 22:20:08 +0800

    2
    2

--- frontEnd/demo/src/PublicComponents/ExperimentList/index.js
+++ frontEnd/demo/src/PublicComponents/ExperimentList/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import GGEditor from '@src';
-import { Table, Button, Row, Col, message } from 'antd';
+import { Table, Button, Row, Col, message, Modal, Input, Icon } from 'antd';
 import { fetchTool } from '../../FetchTool';
 import FlowMinimap from './FlowMinimap';
+const { confirm } = Modal;
 
 class ExperimentList extends React.Component {
 
@@ -11,6 +12,11 @@ class ExperimentList extends React.Component {
 		selectedRowKeys: null,
 		minimapInfo: null,
 		experiment: null,
+		//下面为搜索服务
+		filterDropdownVisible: false,
+		searchText: '',
+		filtered: false,
+		searchData:[],
 	}
 
 	fetchModalList = async () => {
@@ -29,8 +35,12 @@ class ExperimentList extends React.Component {
 	}
 
 	async componentWillMount() {
+		const data =  await this.fetchModalList();
+		console.log(data)
+		console.log("data!")
 		this.setState({
-			dataSource: await this.fetchModalList()
+			dataSource: data,
+			searchData:data,
 		})
 	}
 
@@ -70,40 +80,64 @@ class ExperimentList extends React.Component {
 			return res.data
 		}
 	}
+	getDeleteSearchData=(experimentId)=>{
+		const searchData=this.state.searchData;
+		for (let index in searchData) {
+			const datatemp = searchData[index];
+			if (datatemp.experimentId === experimentId) {
+				searchData.splice(index,1);
+				this.setState({searchData})
+			}
+		}
+	}
 
-	// 点击删除时
+	//点击删除的处理
 	handleDeleteButton = () => {
 		const selectTitleDelte = this.state.selectedRowKeys;
-
+		const self = this;
+		
 		if (selectTitleDelte === null) {
 			message.warning("请先选择要删除的选项");
 		}
 		else {
-			var { dataSource, experiment } = this.state;
-			dataSource.splice(selectTitleDelte[0], 1);
-			this.deleteModal(experiment.experimentId);
-			this.setState({ dataSource });
+			var { dataSource, experiment} = self.state;
+			const titleKey = dataSource[selectTitleDelte].title;
+			console.log("titleKey");
+			console.log(JSON.stringify(titleKey));
+			const experimentId=dataSource[selectTitleDelte].experimentId;
+			confirm({
+				title: 'Do you Want to delete this item?',
+				content: titleKey,
+				onOk() {
+					self.getDeleteSearchData(experimentId);
+					dataSource.splice(selectTitleDelte[0], 1);
+					self.deleteModal(experiment.experimentId);
+					self.setState({ dataSource, selectedRowKeys: null });
+				}
+			})
 		}
 	}
 
 	fetchModalStream = async (experiment) => {
-        const init = {
-            method: 'GET',
-            mode: 'cors',
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded;charset=utf-8"
-            },
-            credentials: 'include'
-        }
-        const res = await fetchTool(`/experiments/${experiment.experimentId}`, init)
-        if (res.code === 200) {
+		const init = {
+			method: 'GET',
+			mode: 'cors',
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded;charset=utf-8"
+			},
+			credentials: 'include'
+		}
+		const res = await fetchTool(`/experiments/${experiment.experimentId}`, init)
+		if (res.code === 200) {
 			console.log(res.data)
-            return res.data
-        }
-    }
+			return res.data
+		}
+	}
 
 	onRowClick = async (record, index) => {
+
 		const experiment = this.state.experiment;
+        console.log(record.title+"ll")
 		const nextExperiment = this.getExperimentByText(record.title);
 
 		if (experiment !== null &&
@@ -117,34 +151,91 @@ class ExperimentList extends React.Component {
 		})
 	}
 
+	onSelectChange = (selectedRowKeys, selectedRows) => {
+		this.onRowClick(selectedRows[0], selectedRowKeys[0])
+	};
+
+	onInputChange = (e) => {
+		this.setState({ searchText: e.target.value });
+	}
+	onSearch = () => {
+		const {searchText} = this.state;
+		const data = this.state.searchData;
+		const reg = new RegExp(searchText, 'gi');
+		this.setState({
+			filterDropdownVisible: false,
+			filtered: !!searchText,
+			dataSource: data.map((record) => {
+				 const match = record.title.match(reg);
+				if (!match) {
+					return null;
+				}
+				return {
+					...record,
+					key:record.title,
+					title: (
+						<span>
+							{record.title.split(reg).map((item, i) => (
+								i > 0 ? [<span className={record.title} style={{ color: "red" }}>{match[0]}</span>, item] : item
+							))}
+						</span>
+					),
+				};
+			}).filter(record => !!record),
+		});
+	}
+
+	getColumnSearchProps = () => ({
+		filterDropdown: (
+			<div className="custom-filter-dropdown">
+				<Input
+					ref={ele => this.searchInput = ele}
+					placeholder="Search name"
+					value={this.state.searchText}
+					onChange={this.onInputChange}
+					onPressEnter={this.onSearch}
+				/>
+				<Button type="primary" onClick={this.onSearch}>Search</Button>
+			</div>
+		),
+		filterIcon: <Icon type="search" style={{ color: this.state.filtered ? '#108ee9' : '#aaa' }} />,
+		filterDropdownVisible: this.state.filterDropdownVisible,
+		onFilterDropdownVisibleChange: (visible) => {
+			this.setState({
+				filterDropdownVisible: visible,
+			}, () => this.searchInput.focus());
+		},
+	})
+
+
 	render() {
 
 		const { dataSource } = this.state;
 		let data = new Array();
+		
 		dataSource.map((item) => {
 			data.push({
 				title: item.title,
 				description: item.description,
 			})
 		})
-
 		const columns = [
 			{
 				title: "title",
 				dataIndex: 'title',
-				width: 100,
+			    ...this.getColumnSearchProps(),
 				render: text => <a onClick={this.handleEnterModel.bind(this, text)} >{text}</a>,
 			},
 			{
 				title: 'description',
 				dataIndex: 'description',
-				width: 100,
 			},
 		]
 
 		const { selectedRowKeys, minimapInfo } = this.state;
 		const rowSelection = {
 			type: "radio",
+			onChange: this.onSelectChange,
 			selectedRowKeys
 		};
 
