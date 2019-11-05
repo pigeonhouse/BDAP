a9acf8c51330a75c6d366e7eca55ac512ae270aa
skrkuromi <987186163@qq.com>
2019-11-05 22:30:10 +0800

    -

--- administerUi/src/NewColumnStyle.js
+++ administerUi/src/NewColumnStyle.js
@@ -1,7 +1,8 @@
 import React, { Fragment } from 'react';
-import { Form, Input, Tooltip, Icon, Row, Col } from 'antd';
+import { Form, Input, Row, Col, Select } from 'antd';
 
 const { Item } = Form;
+const { Option } = Select;
 
 const formItemLayout = {
     labelCol: {
@@ -14,41 +15,41 @@ const formItemLayout = {
     },
 };
 
-class InputStyle extends React.Component {
+class NewColumnStyle extends React.Component {
 
     render() {
         const { getFieldDecorator, index, id, attributes } = this.props;
 
         if (attributes === undefined || attributes[index] === undefined || attributes[index].styleType === undefined) return <Fragment></Fragment>
 
-        if (attributes[index].styleType === 'Input')
+        if (attributes[index].styleType === 'NewColumn')
             return (
                 <Row key={id} >
                     <Col span={4} ></Col>
                     <Col span={8} >
                         <Item
-                            label={
-                                <span>正则式&nbsp;
-						            <Tooltip title="输入值所满足的正则式">
-                                        <Icon type="question-circle-o" />
-                                    </Tooltip>
-                                </span>
-                            }
+                            label="新增列类型"
                             {...formItemLayout}
                             key={`${id}_0`}
                         >
-                            {getFieldDecorator(`attributes[${index}][style][regexp]`, {
+                            {getFieldDecorator(`attributes[${index}][style][newColType]`, {
                                 validateTrigger: ['onChange', 'onBlur'],
                                 rules: [
                                     {
                                         required: true,
                                         whitespace: true,
-                                        message: "输入值所满足的正则式",
+                                        message: "请选择新增类的类型",
                                     },
                                 ],
-                            })(<Input></Input>)}
+                            })(<Select>
+                                <Option value="String">String</Option>
+                                <Option value="Boolean">Boolean</Option>
+                                <Option value="Int">Int</Option>
+                                <Option value="Double">Double</Option>
+                                <Option value="Array[String]">Array[String]</Option>
+                                <Option value="Vector">Vector</Option>
+                            </Select>)}
                         </Item>
-
                     </Col>
                     <Col span={8} >
                         <Item
@@ -69,10 +70,10 @@ class InputStyle extends React.Component {
                         </Item>
                     </Col>
                     <Col span={4} ></Col>
-            </Row>
+                </Row>
             );
         else return <Fragment></Fragment>
     }
 }
 
-export default InputStyle;
+export default NewColumnStyle;
