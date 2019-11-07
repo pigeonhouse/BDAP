a403290bb404f942f98939cc6254a77d68f63f0a
ddrid <2821566237@qq.com>
2019-11-07 19:17:59 +0800

    Update BinaryClassifyEvaluator.scala

--- bdap/experiment-service/src/main/resources/static/evaluation/BinaryClassifyEvaluator.scala
+++ bdap/experiment-service/src/main/resources/static/evaluation/BinaryClassifyEvaluator.scala
@@ -26,43 +26,82 @@ object BinaryClassifyEvaluator {
 
     val auROC = metrics.areaUnderROC
 
+    //    val roc = metrics.roc
+    //    var rocList: util.List[Any] = new util.ArrayList[Any]()
+    //    roc.collect().foreach{c => {
+    //      var rocMap = new util.HashMap[String, Double]()
+    //      rocMap.put("FPR", c._1)
+    //      rocMap.put("TPR", c._2)
+    //      rocList.add(rocMap)
+    //      }
+    //    }
+
     val roc = metrics.roc
-    var rocList: util.List[Any] = new util.ArrayList[Any]()
+    val rocJson = new JSONObject()
+    var fprList: util.List[Double] = new util.ArrayList[Double]()
+    var tprList: util.List[Double] = new util.ArrayList[Double]()
     roc.collect().foreach{c => {
-      var rocMap = new util.HashMap[String, Double]()
-      rocMap.put("FPR", c._1)
-      rocMap.put("TPR", c._2)
-      rocList.add(rocMap)
-      }
+      fprList.add(c._1)
+      tprList.add(c._2)
     }
+    }
+    rocJson.put("FPR",fprList)
+    rocJson.put("TPR",tprList)
+
+
+    //    val prc = metrics.pr
+    //    var prcList: util.List[Any] = new util.ArrayList[Any]()
+    //    prc.collect().foreach{c => {
+    //      var prcMap = new util.HashMap[String, Double]()
+    //      prcMap.put("Recall", c._1)
+    //      prcMap.put("Precision", c._2)
+    //      prcList.add(prcMap)
+    //      }
+    //    }
 
     val prc = metrics.pr
-    var prcList: util.List[Any] = new util.ArrayList[Any]()
+    val prcJson = new JSONObject()
+    var recallList: util.List[Double] = new util.ArrayList[Double]()
+    var precisionList: util.List[Double] = new util.ArrayList[Double]()
     prc.collect().foreach{c => {
-      var prcMap = new util.HashMap[String, Double]()
-      prcMap.put("Recall", c._1)
-      prcMap.put("Precision", c._2)
-      prcList.add(prcMap)
-      }
+      recallList.add(c._1)
+      precisionList.add(c._2)
     }
+    }
+    prcJson.put("Recall",recallList)
+    prcJson.put("Precision",precisionList)
+
+
+    //    val f1Score = metrics.fMeasureByThreshold
+    //    var f1ScoreList: util.List[Any] = new util.ArrayList[Any]()
+    //    f1Score.collect().foreach{c => {
+    //      var f1ScoreMap = new util.HashMap[String, Double]()
+    //      f1ScoreMap.put("Threshold", c._1)
+    //      f1ScoreMap.put("F-score", c._2)
+    //      f1ScoreList.add(f1ScoreMap)
+    //      }
+    //    }
 
     val f1Score = metrics.fMeasureByThreshold
-    var f1ScoreList: util.List[Any] = new util.ArrayList[Any]()
-    f1Score.collect().foreach{c => {
-      var f1ScoreMap = new util.HashMap[String, Double]()
-      f1ScoreMap.put("Threshold", c._1)
-      f1ScoreMap.put("F_score", c._2)
-      f1ScoreList.add(f1ScoreMap)
-      }
+    val f1ScoreJson = new JSONObject()
+    var thresholdList: util.List[Double] = new util.ArrayList[Double]()
+    var fScoreList: util.List[Double] = new util.ArrayList[Double]()
+    roc.collect().foreach{c => {
+      thresholdList.add(c._1)
+      fScoreList.add(c._2)
     }
+    }
+    f1ScoreJson.put("Threshold",fprList)
+    f1ScoreJson.put("F-score",tprList)
+
 
     val jsonObject = new JSONObject()
-    jsonObject.put("method", "BinaryClassifyEvaluator")
+    //jsonObject.put("method", "BinaryClassifyEvaluator")
     jsonObject.put("auPRC", auPRC)
     jsonObject.put("auROC", auROC)
-    jsonObject.put("ROC", rocList)
-    jsonObject.put("PRC", prcList)
-    jsonObject.put("f1Score", f1ScoreList)
+    jsonObject.put("ROC", rocJson)
+    jsonObject.put("PRC", prcJson)
+    jsonObject.put("f1Score", f1ScoreJson)
     val evaluationResult = jsonObject.toJSONString
     println(jsonString)
 
