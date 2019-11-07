d37255075e2b91bb2adeacfbc12dfea8da63afaa
ddrid <2821566237@qq.com>
2019-11-07 21:35:10 +0800

    -

--- bdap/experiment-service/src/main/resources/static/evaluation/BinaryClassifyEvaluator.scala
+++ bdap/experiment-service/src/main/resources/static/evaluation/BinaryClassifyEvaluator.scala
@@ -13,12 +13,13 @@ object BinaryClassifyEvaluator {
     import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
     import com.alibaba.fastjson.JSONObject
     import java.util
+    import scala.collection.JavaConverters._
 
     val metaData = input
 
     val evaluateData = metaData.select("Probability_", labelCol(0)).rdd
 
-    val evaluateRDD: RDD[(Double, Double)] = evaluateData.map{ case r: Row => (r(0).toString().toDouble, r(1).toString().toDouble)}
+    val evaluateRDD: RDD[(Double, Double)] = evaluateData.map { case r: Row => (r(0).toString().toDouble, r(1).toString().toDouble) }
 
     val metrics = new BinaryClassificationMetrics(evaluateRDD)
 
@@ -26,84 +27,30 @@ object BinaryClassifyEvaluator {
 
     val auROC = metrics.areaUnderROC
 
-    //    val roc = metrics.roc
-    //    var rocList: util.List[Any] = new util.ArrayList[Any]()
-    //    roc.collect().foreach{c => {
-    //      var rocMap = new util.HashMap[String, Double]()
-    //      rocMap.put("FPR", c._1)
-    //      rocMap.put("TPR", c._2)
-    //      rocList.add(rocMap)
-    //      }
-    //    }
 
-    val roc = metrics.roc
-    val rocJson = new JSONObject()
-    var fprList: util.List[Double] = new util.ArrayList[Double]()
-    var tprList: util.List[Double] = new util.ArrayList[Double]()
-    roc.collect().foreach{c => {
-      fprList.add(c._1)
-      tprList.add(c._2)
+    def getVisualInfo(tupleList: RDD[(Double, Double)], xlabel: String, ylabel: String): JSONObject = {
+      val valueList: util.List[Any] = new util.ArrayList[Any]
+      tupleList.collect().foreach(t => {
+        val value: util.List[Double] = new util.ArrayList[Double]
+        value.add(t._1)
+        value.add(t._2)
+        valueList.add(value)
+      })
+      val json = new JSONObject()
+      json.put("xlabel",xlabel)
+      json.put("ylabel",ylabel)
+      json.put("value",valueList)
+      json
     }
-    }
-    rocJson.put("FPR",fprList)
-    rocJson.put("TPR",tprList)
 
-
-    //    val prc = metrics.pr
-    //    var prcList: util.List[Any] = new util.ArrayList[Any]()
-    //    prc.collect().foreach{c => {
-    //      var prcMap = new util.HashMap[String, Double]()
-    //      prcMap.put("Recall", c._1)
-    //      prcMap.put("Precision", c._2)
-    //      prcList.add(prcMap)
-    //      }
-    //    }
-
-    val prc = metrics.pr
-    val prcJson = new JSONObject()
-    var recallList: util.List[Double] = new util.ArrayList[Double]()
-    var precisionList: util.List[Double] = new util.ArrayList[Double]()
-    prc.collect().foreach{c => {
-      recallList.add(c._1)
-      precisionList.add(c._2)
-    }
-    }
-    prcJson.put("Recall",recallList)
-    prcJson.put("Precision",precisionList)
-
-
-    //    val f1Score = metrics.fMeasureByThreshold
-    //    var f1ScoreList: util.List[Any] = new util.ArrayList[Any]()
-    //    f1Score.collect().foreach{c => {
-    //      var f1ScoreMap = new util.HashMap[String, Double]()
-    //      f1ScoreMap.put("Threshold", c._1)
-    //      f1ScoreMap.put("F-score", c._2)
-    //      f1ScoreList.add(f1ScoreMap)
-    //      }
-    //    }
-
-    val f1Score = metrics.fMeasureByThreshold
-    val f1ScoreJson = new JSONObject()
-    var thresholdList: util.List[Double] = new util.ArrayList[Double]()
-    var fScoreList: util.List[Double] = new util.ArrayList[Double]()
-    roc.collect().foreach{c => {
-      thresholdList.add(c._1)
-      fScoreList.add(c._2)
-    }
-    }
-    f1ScoreJson.put("Threshold",fprList)
-    f1ScoreJson.put("F-score",tprList)
-
-
-    val jsonObject = new JSONObject()
-    //jsonObject.put("method", "BinaryClassifyEvaluator")
-    jsonObject.put("auPRC", auPRC)
-    jsonObject.put("auROC", auROC)
-    jsonObject.put("ROC", rocJson)
-    jsonObject.put("PRC", prcJson)
-    jsonObject.put("f1Score", f1ScoreJson)
-    val evaluationResult = jsonObject.toJSONString
-    println(jsonString)
+    val evaluationJson = new JSONObject()
+    evaluationJson.put("auPRC", auPRC)
+    evaluationJson.put("auROC", auROC)
+    evaluationJson.put("ROC", getVisualInfo(metrics.roc,"TPR","FPR"))
+    evaluationJson.put("PRC", getVisualInfo(metrics.pr,"Recall","Precision"))
+    evaluationJson.put("f1Score", getVisualInfo(metrics.fMeasureByThreshold,"Threshold","F-score"))
+    val evaluationResult = evaluationJson.toJSONString
+    println(evaluationResult)
 
   }
 }
