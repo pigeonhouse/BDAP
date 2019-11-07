a427ba8271cd214d22b1b426ea9f40b8acda8469
skrkuromi <987186163@qq.com>
2019-11-07 11:26:42 +0800

    -

--- frontEnd/demo/src/PublicComponents/EditorContextMenu/ModelEvaluation/index.js
+++ frontEnd/demo/src/PublicComponents/EditorContextMenu/ModelEvaluation/index.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import { Modal,  Icon, Collapse } from 'antd';
-import { withPropsAPI } from '@src';
-import {
-	Command,
-} from '@src';
+import { Modal, Icon, Collapse } from 'antd';
+import { withPropsAPI, Command } from '@src';
+
+import { fetchTool } from '../../../FetchTool';
 import styles from '../index.less';
 import BinaryEvaluation from './EvaluationType/BinaryClassifyEvaluation';
 import MultiClassifyEvaluation from './EvaluationType/MultiClassifyEvaluation'
@@ -13,27 +12,46 @@ import MultiClassifyEvaluation from './EvaluationType/MultiClassifyEvaluation'
 class ModelEvaluation extends React.Component {
 	state = {
 		visible: false,
-		MlEvaluteVisible: false,
 		evaluation: [[]],
 		col: [],
 		data: [],
-		
+
 	}
-//处理调出页面的ok事件
+	showModal = async () => {
+		const { propsAPI } = this.props;
+		const { getSelected } = propsAPI;
+		const item = getSelected()[0];
+
+		const url = `/experiment-service/flow/node/evaluation/${item.id}_0`;
+		const init = {
+			method: 'GET',
+			mode: 'cors',
+			headers: {
+				"Content-Type": "application/json;charset=utf-8"
+			},
+			credentials: 'include'
+		}
+		const response = await fetchTool(url, init);
+		if (response.status === 200) {
+			console.log(await response.text());
+		}
+
+	}
+
+	//处理调出页面的ok事件
 	handleOk = (e) => {
 		this.setState({
 			visible: false,
-			MlEvaluteVisible: false,
-			mode:"",
+			mode: "",
 			col: [],
 			data: []
 		});
 	}
-//处理调出页面的cancel事件
+
+	//处理调出页面的cancel事件
 	handleCancel = (e) => {
 		this.setState({
 			visible: false,
-			MlEvaluteVisible: false,
 			col: [],
 			data: []
 		});
@@ -43,51 +61,38 @@ class ModelEvaluation extends React.Component {
 		const { getSelected } = propsAPI;
 		const item = getSelected()[0];
 		const currentNode = item.getModel();
-		if(currentNode.keyConfig.state_icon_url=="https://gw.alipayobjects.com/zos/rmsportal/uZVdwjJGqDooqKLKtvGA.svg")
-		{
+		if (currentNode.keyConfig.state_icon_url == "https://gw.alipayobjects.com/zos/rmsportal/uZVdwjJGqDooqKLKtvGA.svg") {
 			alert("该组件未执行，请执行后再进行模型评估！")
 		}
-		else if (currentNode.groupName.label == "模型评估"|| currentNode.groupName.label == "评估") {		
-			this.setState({mode:currentNode.labelName.label,MlEvaluteVisible:true});
-			
-		}
+		else if (currentNode.groupName.label == "模型评估" || currentNode.groupName.label == "评估") {
+			this.setState({ mode: currentNode.labelName.label, visible: true });
 
+		}
 		else alert("该组件非模型评估组件，无法进行模型评估！")
 	}
-   
-  
+
 	render() {
 		return (
-
-            <div>
-                 <Command name="showpicture">
-
-						<div className={styles.item} onClick={this.modelEvaluation}>
-							<Icon type="solution" />
-							<span>模型评估</span>
-						</div>
+			<div>
+				<Command name="showpicture">
+					<div className={styles.item} onClick={this.showModal}>
+						<Icon type="form" />
+						<span>评估结果</span>
+					</div>
 				</Command>
-				<Modal title="模型评估" visible={this.state.MlEvaluteVisible}
+				<Modal title="评估结果" visible={this.state.visible}
 					onOk={this.handleOk} onCancel={this.handleCancel} width={700}
 				>
-					
-					{( ()=>{
-                   switch(this.state.mode)
-	   				{
-		  				 case "二元评估":
-	 					 return <BinaryEvaluation visible={this.state.MlEvaluteVisible}/>
-						 
-						 case "多分类评估":
-						 return <MultiClassifyEvaluation visible={this.state.MlEvaluteVisible}/>
-					
-		  
-	 
-	   				}
-                    }
-				)()}
-			
-				</Modal>
+					{/* {(() => {
+						switch (this.state.mode) {
+							case "二元评估":
+								return <BinaryEvaluation visible={this.state.MlEvaluteVisible} />
 
+							case "多分类评估":
+								return <MultiClassifyEvaluation visible={this.state.MlEvaluteVisible} />
+						}
+					})()} */}
+				</Modal>
 			</div>
 		);
 	}
