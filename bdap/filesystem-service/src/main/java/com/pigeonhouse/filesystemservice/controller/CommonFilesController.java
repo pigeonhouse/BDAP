ecd85bb08783fd7242c9b4641a9f335388800ac3
xty <35415330+intellxty@users.noreply.github.com>
2020-08-02 14:39:37 +0800

    -

--- bdap/filesystem-service/src/main/java/com/pigeonhouse/filesystemservice/controller/CommonFilesController.java
+++ bdap/filesystem-service/src/main/java/com/pigeonhouse/filesystemservice/controller/CommonFilesController.java
@@ -9,7 +9,7 @@ import com.pigeonhouse.filesystemservice.util.TokenParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;行·
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CommonFilesController {
