8c5cc4d9ef935112a67b802182f2cda2e5962263
xty <35415330+intellxty@users.noreply.github.com>
2020-09-01 23:34:58 +0800

    Update webpack.config.demo.dev.js

--- frontEnd/tools/webpack.config.demo.dev.js
+++ frontEnd/tools/webpack.config.demo.dev.js
@@ -4,6 +4,27 @@ const baseConfig = require('./webpack.config.base');
 
 const mode = 'development';
 
+const os = require('os');
+ 
+function getNetworkIp() {
+	let needHost = ''; // 打开的host
+	try {
+		// 获得网络接口列表
+		let network = os.networkInterfaces();
+		for (let dev in network) {
+			let iface = network[dev];
+			for (let i = 0; i < iface.length; i++) {
+				let alias = iface[i];
+				if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+					needHost = alias.address;
+				}
+			}
+		}
+	} catch (e) {
+		needHost = 'localhost';
+	}
+	return needHost;
+}
 const entry = {
   bundle: path.resolve(__dirname, '..', 'demo/src/index.js'),
 };
@@ -39,6 +60,7 @@ const devServer = {
   contentBase: path.resolve(__dirname, '..', 'demo'),
   publicPath: '/dist',
   disableHostCheck: true,
+  host:getNetworkIp()
 };
 
 const output = {
