0a78093611741cb727991900c4b1da9a79c8e0c0
xty <35415330+intellxty@users.noreply.github.com>
2020-09-03 19:14:05 +0800

    Update ServerService.java
    修复当某一livy-server崩溃时系统连带崩溃的问题

--- bdap/livy-service/src/main/java/com/pigeonhouse/livyservice/service/ServerService.java
+++ bdap/livy-service/src/main/java/com/pigeonhouse/livyservice/service/ServerService.java
@@ -1,6 +1,8 @@
 package com.pigeonhouse.livyservice.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -9,8 +11,10 @@ import java.io.IOException;
 import java.util.LinkedHashMap;
 
 
+
 @Service
 public class ServerService {
+    Logger logger = LoggerFactory.getLogger(ServerService.class);
     @Value("${livyAddr}")
     String[] livyAddrList;
 
@@ -24,18 +28,28 @@ public class ServerService {
         int minIndex = -1;
         for (int i = 0; i < livyAddrList.length; i++) {
             RestTemplate restTemplate = new RestTemplate();
-            String res = restTemplate.getForObject("http://" + livyAddrList[i] + "/sessions", String.class);
+            String res="";
+           try {
+               res = restTemplate.getForObject("http://" + livyAddrList[i] + "/sessions", String.class);
+           }
+           catch (Exception e)
+           {
+               logger.warn("A livy-server unconnect :{}","http://" + livyAddrList[i] + "/sessions");
+           }
             LinkedHashMap livySessionDescription = null;
-            try {
-                livySessionDescription = new ObjectMapper().readValue(res, LinkedHashMap.class);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            int num = (int) livySessionDescription.get("total");
-            if (num < minNumber) {
-                minNumber = num;
-                minIndex = i;
-            }
+           if(!res.isEmpty()) {
+               try {
+                   livySessionDescription = new ObjectMapper().readValue(res, LinkedHashMap.class);
+               } catch (IOException e) {
+                   e.printStackTrace();
+               }
+
+               int num = (int) livySessionDescription.get("total");
+               if (num < minNumber) {
+                   minNumber = num;
+                   minIndex = i;
+               }
+           }
         }
         return livyAddrList[minIndex];
     }
