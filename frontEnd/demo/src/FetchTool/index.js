5593af2465668164480ed87d2c31bf16d45eb852
xty <35415330+intellxty@users.noreply.github.com>
2020-09-25 11:17:07 +0800

    Update index.js

--- frontEnd/demo/src/FetchTool/index.js
+++ frontEnd/demo/src/FetchTool/index.js
@@ -33,7 +33,7 @@ export async function fetchTool(url, init) {
         }
         newUrl = "https://result.eolinker.com/MSwz6fu34b763a21e1f7efa84a86a16f767a756952d0f95?uri=localhost:1001" + frontUrl;
     } else if (mode === 'backEndTest') {
-        newUrl = "http://localhost:1001" + url;
+        newUrl = "http://10.105.222.91:1001" + url;
     } else if (mode === "production") {
 
         // 待定url前缀
@@ -97,7 +97,7 @@ async function refreshAccessToken() {
     if (mode === 'frontEndTest') {
         url = "https://result.eolinker.com/MSwz6fu34b763a21e1f7efa84a86a16f767a756952d0f95?uri=localhost:1001" + newUrl;
     } else if (mode === 'backEndTest') {
-        url = "http://localhost:1001" + url;
+        url = "http://10.105.222.91:1001" + url;
     } else if (mode === "production") {
         // 待定url前缀
         url = "https://result.eolinker.com/MSwz6fu34b763a21e1f7efa84a86a16f767a756952d0f95?uri=localhost:1001" + url;
